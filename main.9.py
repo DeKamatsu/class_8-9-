385f0b01ba0134b44f4fdd3c8bcd0f65ca3ca92c
Denis Matveev <denis_matveev@tut.by>
2022-03-23 21:20:06 +0300

    Plus some samples

--- main.9.py
+++ main.9.py
@@ -14,5 +14,24 @@ rex.say()
 tom = Kitty()
 tom.say()
 
-d = re.findall(r"\d{3}", 'hdkdtk2465724 yryj4')
+d = re.findall(r"\d{3}", 'hdkdtk2465724 yryj4')  # возвращает лист (список) с результатами
 print(d)
+
+c = re.compile(r"\d{3}")
+st = 'hdkdtk2465724 yryj4'
+print(re.findall(c, st))  # возвращает лист (список) с результатами
+
+
+import itertools
+
+a = itertools.count(1, 20)
+for i in a:
+    print(i)
+    if i >100:
+        break
+
+x = itertools.combinations("sgadgehz245", 10)
+for i in x:
+    print(i)
+
+
